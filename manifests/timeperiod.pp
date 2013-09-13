06f5cd090a6ce289d02e0d95a8edb77eb3abb2b0
Jeremy Kitchen <kitchen@kitchen.io>
2013-09-13 09:08:56 +0000

    nagios_timeperiod wrapper

---
+++ manifests/timeperiod.pp
@@ -0,0 +1,58 @@
+# == Define: icinga::host
+#
+# wraps nagios_timeperiod for icinga specific bits
+#
+# === Parameters
+#
+# everything nagios_timeperiod supports, including the target
+#
+# === Authors
+#
+# Jeremy Kitchen <jeremy@nationbuilder.com>
+#
+# === Copyright
+#
+# Copyright 2013 3dna Corp
+#
+define icinga::timeperiod (
+  $timeperiod_name = $name,
+  $target          = "${icinga::server::params::objects_dir}/timeperiod_${name}_.cfg",
+  $ensure          = undef,
+  $icinga_alias    = undef,
+  $exclude         = undef,
+  $friday          = undef,
+  $monday          = undef,
+  $register        = undef,
+  $saturday        = undef,
+  $sunday          = undef,
+  $thursday        = undef,
+  $tuesday         = undef,
+  $use             = undef,
+  $wednesday       = undef,
+
+) {
+  include icinga::server::params
+
+  file { $target:
+    owner   => $icinga::server::params::icinga_user,
+    mode    => 0644,
+    require => Nagios_timeperiod[$name],
+  }
+
+  nagios_timeperiod { $name:
+    target          => $target,
+    timeperiod_name => $timeperiod_name,
+    ensure          => $ensure,
+    alias           => $icinga_alias,
+    exclude         => $exclude,
+    friday          => $friday,
+    monday          => $monday,
+    register        => $register,
+    saturday        => $saturday,
+    sunday          => $sunday,
+    thursday        => $thursday,
+    tuesday         => $tuesday,
+    use             => $use,
+    wednesday       => $wednesday,
+  }
+}
