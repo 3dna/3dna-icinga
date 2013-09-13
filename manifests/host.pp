71f0afb456f58ea0d2fd66011cc4b7fca776f3b1
Jeremy Kitchen <kitchen@kitchen.io>
2013-09-13 08:40:27 +0000

    a bunch more options

--- manifests/host.pp
+++ manifests/host.pp
@@ -20,10 +20,23 @@
 # Copyright 2013 3dna Corp
 #
 define icinga::host (
-  $address,
+  $address = undef,
   $target = "${icinga::server::params::objects_dir}/${name}_host.cfg",
-  $max_check_attempts = 3,
-  $alias = $name,
+  $alias = undef,
+  $notifications_enabled = undef,
+  $event_handler_enabled = undef,
+  $flap_detection_enabled = undef,
+  $process_perf_data = undef,
+  $retain_status_information = undef,
+  $retain_nonstatus_information = undef,
+  $check_command = undef,
+  $max_check_attempts = undef,
+  $notification_interval = undef,
+  $notification_period = undef,
+  $notification_options = undef,
+  $contact_groups = undef,
+  $register = undef,
+  $use = undef,
 ) {
   include icinga::server::params
 
@@ -34,9 +47,22 @@ define icinga::host (
   }
 
   nagios_host { $name:
-    target             => $target,
-    max_check_attempts => $max_check_attempts,
-    address            => $address,
-    alias              => $alias,
+    target                       => $target,
+    address                      => $address,
+    alias                        => $alias,
+    notificatios_enabled         => $notifications_enabled,
+    event_handler_enabled        => $event_handler_enabled,
+    flap_detection_enabled       => $flap_detection_enabled,
+    process_perf_data            => $process_perf_data,
+    retain_status_information    => $retain_status_information,
+    retain_nonstatus_information => $retain_nonstatus_information,
+    check_command                => $check_command,
+    max_check_attempts           => $max_check_attempts,
+    notification_interval        => $notification_interval,
+    notification_period          => $notification_period,
+    notification_options         => $notification_options,
+    contact_groups               => $contact_groups,
+    register                     => $register,
+    use                          => $use,
   }
 }
