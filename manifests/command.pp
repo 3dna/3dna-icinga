9a5fdb4f9fa57a15f5923e264d72699a8898defe
Jeremy Kitchen <kitchen@kitchen.io>
2013-09-13 12:28:27 +0000

    wrapper around nagios_command

---
+++ manifests/command.pp
@@ -0,0 +1,41 @@
+# == Define: icinga::command
+#
+# wraps nagios_command for icinga specific bits
+#
+# === Parameters
+#
+# everything nagios_command supports, including the target
+#
+# === Authors
+#
+# Jeremy Kitchen <jeremy@nationbuilder.com>
+#
+# === Copyright
+#
+# Copyright 2013 3dna Corp
+#
+define icinga::command (
+  $target       = "${icinga::server::params::objects_dir}/command_${name}.cfg",
+  $command_name = undef,
+  $ensure       = undef,
+  $command_line = undef,
+  $poller_tag   = undef,
+  $use          = undef,
+) {
+  include icinga::server::params
+
+  file { $target:
+    owner   => $icinga::server::params::icinga_user,
+    mode    => 0644,
+    require => Nagios_command[$name],
+  }
+
+  nagios_command { $name:
+    target       => $target,
+    notify       => Class['::icinga::server::service'],
+    ensure       => $ensure,
+    command_line => $command_line,
+    poller_tag   => $poller_tag,
+    use          => $use,
+  }
+}
