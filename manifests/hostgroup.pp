4be895dd4af68fe4c4aec24a21967ac545617112
David Lovitch <dlovitch@gmail.com>
2014-11-05 23:10:52 -0800

    Cleaning up some missing pieces of hostgroup.

--- manifests/hostgroup.pp
+++ manifests/hostgroup.pp
@@ -20,19 +20,19 @@
 # Copyright 2014 3dna Corp
 #
 define icinga::hostgroup (
-  $hostgroup_name = $name,
-  $ensure     = undef,
-  $action_url = undef,
-  $icinga_alias = undef,
-  $hostgroup_members = undef,
-  $members = undef,
-  $notes = undof,
-  $notes_url = undef,
-  $provider = undef,
-  $realm = undef,
-  $register = undef,
-  $target       = "${icinga::server::params::objects_dir}/host_${name}.cfg",
-  $use = undef,
+  $hostgroup_name     = $name,
+  $ensure             = undef,
+  $action_url         = undef,
+  $icinga_alias       = undef,
+  $hostgroup_members  = undef,
+  $members            = undef,
+  $notes              = undef,
+  $notes_url          = undef,
+  $provider           = undef,
+  $realm              = undef,
+  $register           = undef,
+  $target             = "${icinga::server::params::objects_dir}/hostgroup_${name}.cfg",
+  $use                = undef,
 ) {
   include icinga::server::params
 
@@ -49,12 +49,13 @@ define icinga::hostgroup (
     alias             => $icinga_alias,
     hostgroup_members => $hostgroup_members,
     members           => $members,
-    notes             => $notes,
+    notes           -  => $notes,
     notes_url         => $notes_url,
     provider          => $provider,
     realm             => $realm,
     register          => $register,
     target            => $target,
     use               => $use,
+    notify            => Class['::icinga::server::configcheck'],
   }
 }
